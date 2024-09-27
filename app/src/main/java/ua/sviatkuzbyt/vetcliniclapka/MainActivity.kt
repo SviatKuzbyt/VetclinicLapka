8525cd7d1ee891ea75a07c57d2d330975c439b20
Sviatoslav <sviatoslav.kuzbyt@gmail.com>
2024-09-27 17:54:02 +0300

    MainActivity: create design

--- app/src/main/java/ua/sviatkuzbyt/vetcliniclapka/MainActivity.kt
+++ app/src/main/java/ua/sviatkuzbyt/vetcliniclapka/MainActivity.kt
@@ -5,15 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ua.sviatkuzbyt.vetcliniclapka.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            binding.mainMenu.setPadding(0, 0, 0, systemBars.bottom)
             insets
         }
     }
